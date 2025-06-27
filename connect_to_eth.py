67f4a682496e469e7d2ec3abe4fdfd24ccc26a26
CarlZhou <wuxi8black@gmail.com>
2025-06-27 18:11:36 +0000

    middleware

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -18,23 +18,24 @@ def connect_to_eth():
 
 
 def connect_with_middleware(contract_json):
-	with open(contract_json, "r") as f:
-		d = json.load(f)
-		d = d['bsc']
-		address = d['address']
-		abi = d['abi']
+    with open(contract_json, "r") as f:
+        d = json.load(f)
+        d = d['bsc']
+        address = d['address']
+        abi = d['abi']
 
-	# TODO complete this method
-	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = 0
+    # First section similar to connect_to_eth but with BNB Chain (Binance Smart Chain) URL
+    bnb_url = ""  # FILL THIS IN with BNB provider URL
+    w3 = Web3(HTTPProvider(bnb_url))
+    assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-	contract = 0
+    # Second section injecting middleware and creating contract object
+    # Binance Smart Chain (BSC) uses Proof-of-Authority consensus, so middleware injection is necessary
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-	return w3, contract
+    contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
 
+    return w3, contract
 
 if __name__ == "__main__":
 	connect_to_eth()
